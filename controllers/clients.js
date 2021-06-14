a9ed97d84413f6daf828d30057d7811aee4db140
Faizan Shah <faizanshah1496@gmail.com>
2021-06-14 22:02:59 +0500

    Minor changes controller and added readme file

--- controllers/clients.js
+++ controllers/clients.js
@@ -33,33 +33,8 @@ exports.createClient = (req, res) => {
     res.status(400).send({ message: 'Content can not be empty!' });
     return;
   }
-  const client = {
-    id: req.body.id,
-    dateCreated: req.body.dateCreated,
-    dateModified: req.body.dateModified,
-    createdBy: req.body.createdBy,
-    createdByName: req.body.createdByName,
-    modifiedBy: req.body.modifiedBy,
-    modifiedByName: req.body.modifiedByName,
-    c_code: req.body.c_code,
-    c_name: req.body.c_name,
-    c_familyName: req.body.c_familyName,
-    c_address: req.body.c_address,
-    c_TelPhoneNo: req.body.c_TelPhoneNo,
-    c_workPhoneNo: req.body.c_workPhoneNo,
-    c_cellNo: req.body.c_cellNo,
-    c_faxNo: req.body.c_faxNo,
-    c_email: req.body.c_email,
-    c_clientSince: req.body.c_clientSince,
-    c_identificationNo: req.body.c_identificationNo,
-    c_badPayer: req.body.c_badPayer,
-    c_maxAmount: req.body.c_maxAmount,
-    c_worksAt: req.body.c_worksAt,
-    c_remarks: req.body.c_remarks,
-    c_status: req.body.c_status,
-  };
   models.app_fd_nnv_clients
-    .create(client)
+    .create(req.body)
     .then((data) => res.json(data))
     .catch((err) => {
       res.status(500).send({
@@ -70,37 +45,13 @@ exports.createClient = (req, res) => {
 };
 
 exports.upsertClient = (req, res) => {
+
   if (!req.body.id) {
     res.status(400).send({ message: 'Content can not be empty!' });
     return;
   }
-  const client = {
-    id: req.body.id,
-    dateCreated: req.body.dateCreated,
-    dateModified: req.body.dateModified,
-    createdBy: req.body.createdBy,
-    createdByName: req.body.createdByName,
-    modifiedBy: req.body.modifiedBy,
-    modifiedByName: req.body.modifiedByName,
-    c_code: req.body.c_code,
-    c_name: req.body.c_name,
-    c_familyName: req.body.c_familyName,
-    c_address: req.body.c_address,
-    c_TelPhoneNo: req.body.c_TelPhoneNo,
-    c_workPhoneNo: req.body.c_workPhoneNo,
-    c_cellNo: req.body.c_cellNo,
-    c_faxNo: req.body.c_faxNo,
-    c_email: req.body.c_email,
-    c_clientSince: req.body.c_clientSince,
-    c_identificationNo: req.body.c_identificationNo,
-    c_badPayer: req.body.c_badPayer,
-    c_maxAmount: req.body.c_maxAmount,
-    c_worksAt: req.body.c_worksAt,
-    c_remarks: req.body.c_remarks,
-    c_status: req.body.c_status,
-  };
   models.app_fd_nnv_clients
-    .upsert(client)
+    .upsert(req.body)
     .then((data) => res.json(data))
     .catch((err) => {
       res.status(500).send({
