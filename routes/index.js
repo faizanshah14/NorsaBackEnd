01702c48f3e67452a46a0630198476adcf09ab02
Faizan Shah <faizanshah1496@gmail.com>
2021-06-26 15:13:27 +0500

    fixed issue with registering apis in router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const authRouter = require('./auth');
 const clientsRouter = require('./clients');
+const merchantsRouter = require('./merchants');
 module.exports = function (app) {
   app.use('/api/auth', authRouter);
   app.use('/api/clients', clientsRouter);
+  app.use('/api/merchants', merchantsRouter);
 };
